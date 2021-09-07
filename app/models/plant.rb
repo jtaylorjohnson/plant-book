2a6575aaf4e4186bf8bca8b01cf2070e66701b43
jtaylorjohnson <jtaylorjohnson@gmail.com>
2021-09-06 19:49:41 -0500

    redo migrations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,6 @@
 class Plant < ApplicationRecord
+    has_many :user_plants
+    has_many :users, through: :user_plants
+    has_many :comments
+    has_many :commented_users, through: :comments, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :user_plants
+    has_many :plants, through: :user_plants
+    has_many :comments
+    has_many :commented_plants, through: :comments, source: :plant
+    has_secure_password
 end
